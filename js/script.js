90e4a6622b343aa05c3f06804a03b5bcf82e8563
Pavel <silvesterkot@gmail.com>
2020-04-19 22:25:33 +1000

    countdown-timer

--- js/script.js
+++ js/script.js
@@ -36,4 +36,49 @@ window.addEventListener('DOMContentLoaded', function () {
     }
   });
 
+  //Timer
+
+  let deadLine = '2018-10-21';
+
+  function getTimeRemaining(endtime) {
+    //получаем разницу
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+      //секунды, минуты, часы. округляем - Math.floor
+      seconds = Math.floor((t / 1000) % 60),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      hours = Math.floor((t / (1000 * 60 * 60)));
+
+    //для дней
+    //hours = Math.floor((t / 1000 / 60 / 60) % 24),
+    //days = Math.floor((t / (1000 * 60 * 60 * 24)));
+
+    return {
+      'total': t,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+      hours = timer.querySelector('.hours'),
+      minutes = timer.querySelector('.minutes'),
+      seconds = timer.querySelector('.seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      let t = getTimeRemaining(endtime);
+      hours.textContent = t.hours;
+      minutes.textContent = t.minutes;
+      seconds.textContent = t.seconds;
+
+      if (t.total < 0) {
+        clearInterval(timeIntervel);
+      }
+    }
+  }
+
+  setClock('timer', deadLine);
+
 });

73ddd746da6cc414ab5f01b44846810dec2ce12e
Pavel <silvesterkot@gmail.com>
2020-04-20 19:48:39 +1000

    countdown-timer

--- js/script.js
+++ js/script.js
@@ -1,84 +1,94 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-  'use strict';
-  let tab = document.querySelectorAll('.info-header-tab'),
-    info = document.querySelector('.info-header'),
-    tabContent = document.querySelectorAll('.info-tabcontent');
+	'use strict';
+	let tab = document.querySelectorAll('.info-header-tab'),
+		info = document.querySelector('.info-header'),
+		tabContent = document.querySelectorAll('.info-tabcontent');
 
-  //скрыть табы
-  function hideTabContent(a) {
-    for (let i = a; i < tabContent.length; i++) {
-      tabContent[i].classList.remove('show');
-      tabContent[i].classList.add('hide');
-    }
+	//скрыть табы
+	function hideTabContent(a) {
+		for (let i = a; i < tabContent.length; i++) {
+			tabContent[i].classList.remove('show');
+			tabContent[i].classList.add('hide');
+		}
 
-  }
+	}
 
-  hideTabContent(1);
+	hideTabContent(1);
 
-  function showTabContent(b) {
-    if (tabContent[b].classList.contains('hide')) {
-      tabContent[b].classList.remove('hide');
-      tabContent[b].classList.add('show');
-    }
-  }
+	function showTabContent(b) {
+		if (tabContent[b].classList.contains('hide')) {
+			tabContent[b].classList.remove('hide');
+			tabContent[b].classList.add('show');
+		}
+	}
 
-  info.addEventListener('click', function (event) {
-    let target = event.target;
-    if (target && target.classList.contains('info-header-tab')) {
-      for (let i = 0; i < tab.length; i++) {
-        if (target == tab[i]) {
-          hideTabContent(0);
-          showTabContent(i);
-          break;
-        }
-      }
-    }
-  });
+	info.addEventListener('click', function (event) {
+		let target = event.target;
+		if (target && target.classList.contains('info-header-tab')) {
+			for (let i = 0; i < tab.length; i++) {
+				if (target == tab[i]) {
+					hideTabContent(0);
+					showTabContent(i);
+					break;
+				}
+			}
+		}
+	});
 
-  //Timer
+	//Timer
 
-  let deadLine = '2018-10-21';
+	let deadLine = '2020-04-21';
 
-  function getTimeRemaining(endtime) {
-    //получаем разницу
-    let t = Date.parse(endtime) - Date.parse(new Date()),
-      //секунды, минуты, часы. округляем - Math.floor
-      seconds = Math.floor((t / 1000) % 60),
-      minutes = Math.floor((t / 1000 / 60) % 60),
-      hours = Math.floor((t / (1000 * 60 * 60)));
+	function getTimeRemaining(endtime) {
+		//получаем разницу
+		let t = Date.parse(endtime) - Date.parse(new Date()),
+			//секунды, минуты, часы. округляем - Math.floor
+			seconds = Math.floor((t / 1000) % 60),
+			minutes = Math.floor((t / 1000 / 60) % 60),
+			hours = Math.floor((t / (1000 * 60 * 60)));
 
-    //для дней
-    //hours = Math.floor((t / 1000 / 60 / 60) % 24),
-    //days = Math.floor((t / (1000 * 60 * 60 * 24)));
+		//для дней
+		//hours = Math.floor((t / 1000 / 60 / 60) % 24),
+		//days = Math.floor((t / (1000 * 60 * 60 * 24)));
 
-    return {
-      'total': t,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    };
-  }
+		return {
+			'total': t,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
 
-  function setClock(id, endtime) {
-    let timer = document.getElementById(id),
-      hours = timer.querySelector('.hours'),
-      minutes = timer.querySelector('.minutes'),
-      seconds = timer.querySelector('.seconds'),
-      timeInterval = setInterval(updateClock, 1000);
+	function setClock(id, endtime) {
+		let timer = document.getElementById(id),
+			hours = timer.querySelector('.hours'),
+			minutes = timer.querySelector('.minutes'),
+			seconds = timer.querySelector('.seconds'),
+			timeInterval = setInterval(updateClock, 1000);
 
-    function updateClock() {
-      let t = getTimeRemaining(endtime);
-      hours.textContent = t.hours;
-      minutes.textContent = t.minutes;
-      seconds.textContent = t.seconds;
+		function updateClock() {
+			let t = getTimeRemaining(endtime);
 
-      if (t.total < 0) {
-        clearInterval(timeIntervel);
-      }
-    }
-  }
+			function addZero(num) {
+				if (num <= 9) {
+					return '0' + num;
+				} else return num;
+			};
 
-  setClock('timer', deadLine);
+			hours.textContent = addZero(t.hours);
+			minutes.textContent = addZero(t.minutes);
+			seconds.textContent = addZero(t.seconds);
 
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+				hours.textContent = '00';
+				minutes.textContent = '00';
+				seconds.textContent = '00';
+			}
+		}
+
+	}
+
+	setClock('timer', deadline);
 });
